f23894981fc25c3de3fd104f03fd1da232542f2e
guyu <gyyyy2019@foxmail.com>
2025-07-10 19:51:32 +0800

    导航栏完成

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,96 @@
+<script lang="ts" setup>
+import { useRouter } from 'vue-router';
+import { h, ref } from 'vue';
+import { HomeOutlined } from '@ant-design/icons-vue';
+import { MenuProps } from 'ant-design-vue';
+const current = ref<string[]>([]);
+const items = ref<MenuProps['items']>([
+  {
+    key: '/',
+    icon: () => h(HomeOutlined),
+    label: '主页',
+    title: '主页',
+  },
+  {
+    key: '/about',
+    label: '关于',
+    title: '关于',
+  },
+  {
+    key: '/others',
+    label: '暂无',
+    title: '暂无',
+  },
+]);
+interface MenuClick {
+    key:string,
+}
+const router = useRouter()
+
+router.afterEach((to , from ,next) => {
+    current.value = [to.path]
+})
+const doMenuClick = ({ key }:MenuClick) => {
+    router.push({
+        path:key,
+    })
+}
+
+
+
+</script>
+
+
+<template>
+  <div id="globalHeader">
+    <RouterLink class="title" to="/">
+        <div class="title-bar">
+            <img src="../assets/logo.png" alt="logo">
+            <span>雨图</span>
+        </div>
+    </RouterLink>
+
+    <a-menu class="menu" v-model:selectedKeys="current" mode="horizontal" :items="items" @click="doMenuClick" />
+    <div class="user-login-status">
+        <a-button type="primary" href="#">登录</a-button>
+    </div>
+  </div>
+  
+</template>
+
+
+<style scoped>
+#globalHeader {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+#globalHeader .menu {
+    flex: 1;
+}
+
+/* 尺寸 */
+#globalHeader .title {
+    width: 80px;
+    height: auto ;
+}
+
+#globalHeader .title .title-bar {
+    display: flex;
+    align-items: center;
+    font-size: 18px;
+    font-weight: bold;
+    color: #584747;
+}
+
+.title .title-bar img {
+    border-radius: 30%;
+    margin-right: 6px;
+}
+
+#globalHeader .user-login-status{
+    width: 70px;
+    margin-right: 10px;
+}
+</style>
---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,64 @@
+<script lang="ts" setup>
+import GlobalHeader from '@/components/GlobalHeader.vue';
+import { RouterLink , RouterView } from 'vue-router';
+</script>
+
+
+<template>
+    <div id="basicLayout">
+        <a-layout class="layout">
+            <a-layout-header class="header">
+                <GlobalHeader></GlobalHeader>
+            </a-layout-header>
+            <a-layout-content class="content">
+                <RouterView></RouterView>
+            </a-layout-content>
+            <a-layout-footer class="footer">
+                <a href="#" target="_blank">
+                    谷雨云图库 by GuGuGuYu
+                </a>
+            </a-layout-footer>
+        </a-layout>
+    </div>
+</template>
+
+
+<style scoped>
+#basicLayout {
+    background-color: #efefef;
+}
+
+.layout .header {
+    background-color: #fff;
+    padding: 0px 10px;
+    color: unset;
+    margin-bottom: 5px;
+}
+
+.layout {
+    /* 100可视高度 ， 铺满视觉 */
+    min-height: 100vh; 
+}
+
+.layout .content {
+    padding: 30px;
+    margin-bottom: 20vh;
+    background-color: #fff;
+}
+
+#basicLayout .footer{
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    height: auto;
+    box-sizing: border-box;
+    background-color: #efefef;
+}
+
+.footer a {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+}
+</style>
